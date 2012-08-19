07f13b19d5eb945cefab97fff35b58f6a91eddec
Zack Wyatt <Zack@Zacks-MacBook-Pro.local>
2012-08-19 14:18:11 -0600

    first commit.

---
+++ Wyatt_Zack_Project4/json.js
@@ -0,0 +1,26 @@
+var json = {
+    "task1": {
+        "task" : ["Task Type:", "School"],
+        "tdate" : ["Date:", "2012-08-23"],
+        "tname" : ["Assign Task:", "Zack Wyatt"],
+        "tcomments" : ["Comments:", "Test"],
+        "trating" : ["Rating:", "10"],
+        "ttopic" : ["Task Topic:", "Project"]
+    },
+    "task2": {
+        "task" : ["Task Type:", "Personal"],
+        "tdate" : ["Date:", "2012-08-25"],
+        "tname" : ["Assign Task:", "Jake Wyatt"],
+        "tcomments" : ["Comments:", "Test"],
+        "trating" : ["Rating:", "5"],
+        "ttopic" : ["Task Topic:", "errand"]
+    },
+     "task3": {
+        "task" : ["Task Type:", "Work"],
+        "tdate" : ["Date:", "2012-08-24"],
+        "tname" : ["Assign Task:", "Angie Hendrix"],
+        "tcomments" : ["Comments:", "Test"],
+        "trating" : ["Rating:", "1"],
+        "ttopic" : ["Task Topic:", "walk the dog"]
+    }
+}
